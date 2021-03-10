4a7f5427c525ed869fe03d38c06ae04a4970c8de
adenova01 <adenovapratamaputra@gmail.com>
2021-03-10 20:29:46 +0700

    commit

---
+++ resources/views/vendor/voyager/resultujians/browse.blade.php
@@ -0,0 +1,7 @@
+@extends('voyager::master')
+
+@section('page_title', __('voyager::generic.viewing').' '.$dataType->getTranslatedAttribute('display_name_plural'))
+
+@section('content')
+    <h1>hasil ujian</h1>
+@stop
