6bac087291bfccd086c36c5dc93e56e312f4ac88
reztsky <sultaninot@gmail.com>
2021-03-17 16:37:25 +0700

    update landing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -283,24 +283,27 @@
                 <div class="flex flex-col items-start justify-start w-full h-full p-10 lg:p-16 xl:p-24">
                     <h4 class="w-full text-3xl font-bold">Signup</h4>
                     <p class="text-lg text-gray-500">or, if you have an account you can <a href="{{url("/tryout")}}" class="text-blue-600 underline">sign in</a></p>
-                    <div class="relative w-full mt-10 space-y-8">
-                        <div class="relative">
-                            <label class="font-medium text-gray-900">Name</label>
-                            <input type="text" class="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" placeholder="Enter Your Name" />
+                    <form action="registrasiUser" method="post">
+                        {{ csrf_field() }}
+                        <div class="relative w-full mt-10 space-y-8">
+                            <div class="relative">
+                                <label class="font-medium text-gray-900">Name</label>
+                                <input type="text" name="name" id="name" value="{{ old('email') }}" class="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" placeholder="Enter Your Name" />
+                            </div>
+                            <div class="relative">
+                                <label class="font-medium text-gray-900">Email</label>
+                                <input type="text" name="email" id="email" value="{{ old('email') }}" class="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" placeholder="Enter Your Email Address" />
+                            </div>
+                            <div class="relative">
+                                <label class="font-medium text-gray-900">Password</label>
+                                <input type="text" name="password" placeholder="{{ __('voyager::generic.password') }}" class="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" placeholder="Password" />
+                            </div>
+                            <div class="relative">
+                                <button type="submit" class="inline-block w-full px-5 py-4 text-lg font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease">Create Account</button>
+                                <a href="#_" class="inline-block w-full px-5 py-4 mt-3 text-lg font-bold text-center text-gray-900 transition duration-200 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 ease">Sign up with Google</a>
+                            </div>
                         </div>
-                        <div class="relative">
-                            <label class="font-medium text-gray-900">Email</label>
-                            <input type="text" class="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" placeholder="Enter Your Email Address" />
-                        </div>
-                        <div class="relative">
-                            <label class="font-medium text-gray-900">Password</label>
-                            <input type="text" class="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" placeholder="Password" />
-                        </div>
-                        <div class="relative">
-                            <a href="#_" class="inline-block w-full px-5 py-4 text-lg font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease">Create Account</a>
-                            <a href="#_" class="inline-block w-full px-5 py-4 mt-3 text-lg font-bold text-center text-gray-900 transition duration-200 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 ease">Sign up with Google</a>
-                        </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
